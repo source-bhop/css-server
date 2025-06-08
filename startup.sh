0d92842e8605c16c7f4f975bb7856801d5f4e982
not-log <56871060+not-log@users.noreply.github.com>
2025-06-08 16:25:37 +0400

    стабилизация сборки

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "🚀 Сервер запущен"
-exec "$CSS_DIR/srcds_run" -game cstrike -console -secure -port 27015 +map de_dust2 +sv_lan 0
+exec "$CSS_DIR/srcds_run" -game cstrike -console -secure -port $PORT +map de_dust2 +sv_lan 0
